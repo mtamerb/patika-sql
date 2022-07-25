074d96207d24d22d21258af0bbce09b098c1cb21
Tamer Bilici <64536219+mtamerb@users.noreply.github.com>
2022-07-25 18:30:46 +0300

    Add files

---
+++ odev3/soru2.sql
@@ -0,0 +1,6 @@
+-- country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+
+SELECT country
+FROM country
+WHERE country like '%n'
+	AND LENGTH (country) >= 6;
