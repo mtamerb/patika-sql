1b131aba35a16e1e3fe782eeba8d9ef6e9688508
Tamer Bilici <64536219+mtamerb@users.noreply.github.com>
2022-07-25 17:41:59 +0300

    Update soru3.sql

--- odev2/soru3.sql
+++ odev2/soru3.sql
@@ -2,9 +2,5 @@
 
 SELECT *
 FROM film
-WHERE rental_rate IN (0.99,
-																							2.99,
-																							4.99)
-	AND replacement_cost IN (12.99,
-																											15.99,
-																											28.99);
+WHERE rental_rate IN (0.99, 2.99, 4.99)
+	AND replacement_cost IN (12.99, 15.99, 28.99);
