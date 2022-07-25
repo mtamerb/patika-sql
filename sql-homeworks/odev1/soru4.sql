c279489785c0663efa20a2deae361061998f9192
Tamer Bilici <64536219+mtamerb@users.noreply.github.com>
2022-07-25 17:40:48 +0300

    Delete directory for edit

--- sql-homeworks/odev1/soru4.sql
+++
@@ -1,5 +0,0 @@
--- customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
-
-SELECT last_name
-FROM customer
-WHERE first_name = 'Mary';
--- sql-homeworks/odev1/soru5.sql
+++
@@ -1,7 +0,0 @@
--- film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
-
-SELECT *
-FROM film
-WHERE NOT LENGTH > 50
-	AND NOT (rental_rate = 2.99
-										OR rental_rate = 4.99);
